59b5209a5c03d740f9eb30810a9fa59919399529
tiden2000 <58420831+tiden2000@users.noreply.github.com>
2020-11-02 20:23:57 +0700

    Added search function

--- WindowsFormsApp3/Form2.Designer.cs
+++ WindowsFormsApp3/Form2.Designer.cs
@@ -38,8 +38,8 @@
             this.printBillToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.textQuantity = new System.Windows.Forms.TextBox();
+            this.buttonSearchQuantity = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -121,27 +121,28 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(68, 100);
+            this.label2.Location = new System.Drawing.Point(78, 103);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(123, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "Inventory Quantity";
             // 
-            // textBox1
+            // textQuantity
             // 
-            this.textBox1.Location = new System.Drawing.Point(197, 100);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(199, 22);
-            this.textBox1.TabIndex = 3;
+            this.textQuantity.Location = new System.Drawing.Point(207, 103);
+            this.textQuantity.Name = "textQuantity";
+            this.textQuantity.Size = new System.Drawing.Size(199, 22);
+            this.textQuantity.TabIndex = 3;
             // 
-            // button1
+            // buttonSearchQuantity
             // 
-            this.button1.Location = new System.Drawing.Point(445, 76);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(163, 70);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Search";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonSearchQuantity.Location = new System.Drawing.Point(431, 100);
+            this.buttonSearchQuantity.Name = "buttonSearchQuantity";
+            this.buttonSearchQuantity.Size = new System.Drawing.Size(158, 28);
+            this.buttonSearchQuantity.TabIndex = 4;
+            this.buttonSearchQuantity.Text = "Search";
+            this.buttonSearchQuantity.UseVisualStyleBackColor = true;
+            this.buttonSearchQuantity.Click += new System.EventHandler(this.buttonSearchQuantity_Click);
             // 
             // dataGridView1
             // 
@@ -159,14 +160,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonSearchQuantity);
+            this.Controls.Add(this.textQuantity);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form2";
             this.Text = "CheckQuantity";
+            this.Load += new System.EventHandler(this.Form2_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -187,8 +189,8 @@
         private System.Windows.Forms.ToolStripMenuItem printBillToolStripMenuItem;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textQuantity;
+        private System.Windows.Forms.Button buttonSearchQuantity;
         private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- WindowsFormsApp3/Form2.cs
+++ WindowsFormsApp3/Form2.cs
@@ -16,7 +16,14 @@ namespace WindowsFormsApp3
         {
             InitializeComponent();
         }
-        
+
+        public DataTable table { get; set; }
+
+        private void Form2_Load(object sender, EventArgs e) {
+            dataGridView1.DataSource = table;
+            dataGridView1.Rows[0].Selected = true;
+        }
+
         private void inputMerchandiseToolStripMenuItem_Click(object sender, EventArgs e)
         {
          
@@ -25,6 +32,21 @@ namespace WindowsFormsApp3
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void buttonSearchQuantity_Click(object sender, EventArgs e) {
+            // Reset all filtered results on datagridview before searching again
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = table;
+            // Loop and check for rows of with "quantity" > user input then hide them to display the remaining rows
+            // as result
+            int quantity = Int32.Parse(textQuantity.Text);
+            int i;
+            for(i = 0; i < dataGridView1.Rows.Count - 1; i = i + 1) {
+                if (Int32.Parse(dataGridView1.Rows[i].Cells[2].Value.ToString()) > quantity) {
+                    dataGridView1.Rows[i].Visible = false;
+                }
+            }
         }
     }
 }
